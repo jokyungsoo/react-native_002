0f03bd0c979393e2e8042678e0c19b643c5886d9
PJO <87128671+jokyungsoo@users.noreply.github.com>
2023-11-17 16:10:11 +0900

    calculator

--- calculator/App.js
+++ calculator/App.js
@@ -3,8 +3,11 @@ import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [input, setInput] = useState();
-    const [currentOperator, setCurrentOperator] = useState(null);
+    const [input, setInput] = useState(); // 입력값 2 -> 14
+    const [currentOperator, setCurrentOperator] = useState(null); // 덧셈 연산자 + -> null
+    const [result, setResult] = useState(null); // 결과값 12 -> 14
+    const [tempInput, setTempIput] = useState(null); // 캐시값 -> 누적값
+    const [tempOpreator, setTempOperator] = useState(null); // + 누적값
 
     return (
         <View style={styles.container}>
